03f4d0c08bb2404bfb8dde3b97026f09cc24a291
Nirav0712 <thedigitalconnect712@gmail.com>
2026-08-06 12:07:56 +0530

    Complete authentication module

--- backend/models/User.js
+++ backend/models/User.js
@@ -41,12 +41,10 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash Password Before Save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   this.password = await bcrypt.hash(this.password, 10);
-
-  next();
 });
 
 // Compare Password
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,10 @@ import express from "express";
 import {
   registerUser,
   loginUser,
+  getProfile,
 } from "../controllers/authController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -14,5 +17,7 @@ router.post("/register", registerUser);
 // Login
 
 router.post("/login", loginUser);
+
+router.get("/profile", protect, getProfile);
 
 export default router;
