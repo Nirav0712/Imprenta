58fd7253013770d77274b14945c59aee57cfcde7
Nirav0712 <thedigitalconnect712@gmail.com>
2026-08-10 10:37:52 +0530

    remove duplicate client folder

--- client/src/hooks/useRecentlyViewed.js
+++
@@ -1,7 +0,0 @@
-import { useRecentlyViewedContext } from "../context/RecentlyViewedContext";
-
-const useRecentlyViewed = () => {
-  return useRecentlyViewedContext();
-};
-
-export default useRecentlyViewed;
